9b89f1f3909cc5d70b60cef8249e27737f380ce1
LUIZ FERNANDO PEREIRA <salgadinhosdofernando@gmail.com>
2021-05-10 21:02:07 -0300

    Primeiro projeto

---
+++ script.js
@@ -0,0 +1,32 @@
+
+document.querySelector(".hamburguer").addEventListener("click", ()=>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector('#layout-sim').addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function(){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector('label[for=prazo]').innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const comLayout = document.querySelector('#layout-sim').checked   
+    const prazo = document.querySelector("#prazo").value 
+    let preco = qtde * 100;
+    if (temJS) {
+        preco = preco *1.1 
+    }
+    if (comLayout) {
+        preco = preco + 500;
+    } 
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed (2)}`  
+}
